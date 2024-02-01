63ab9cd303c77b2bde7389922f13f1441fab9092
DeepanshuKhudania <158448799+DeepanshuKhudania@users.noreply.github.com>
2024-02-01 17:58:47 +0530

    Add files via upload

---
+++ ThirdPattern.java
@@ -0,0 +1,19 @@
+class ThirdPattern{
+	public static void main(String[] args) {
+	int star = 1;
+	int row = 3;
+
+	for (int i=1; i<=row ;i++ ) {
+		for (int j=1;j<=star ;j++ ) {
+			System.out.print("* ");
+			
+		}System.out.println();
+		star++;
+		
+	}
+		
+	}
+
+
+
+}

30b27edd49807c6262445546d45ede5267e854c3
DeepanshuKhudania <158448799+DeepanshuKhudania@users.noreply.github.com>
2024-05-30 21:45:28 +0530

    Add files via upload
    Sort using inbuilt class

---
+++ Sorting.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Sorting {
+
+	public static void main(String[] args) {
+		int [] num = {20,10,30,50,60,70};
+		System.out.println(Arrays.toString(num));
+		Arrays.sort(num);
+		System.out.println(Arrays.toString(num));
+
+	}
+
+}
